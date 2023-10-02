6ca1cf0f6a12948a3191ce836128b89307fb52a5
Ankita <Ankitajb0991@gmail.com>
2023-10-02 13:14:49 +1300

    add test

--- src/test/java/resources/demo.java
+++ src/test/java/resources/demo.java
@@ -4,5 +4,7 @@ public class demo {
     public void postJira()
     {
         System.out.println("firstPostJira");
+        System.out.println("secondPostJira");
+        System.out.println("LastPostJira");
     }
 }
